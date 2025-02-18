0358edbd71cca5762fcd112eddec80859ae96b9f
lesliebear <tianqi.xiong@gmail.com>
2025-02-18 16:31:27 -0500

    updatred blocks and link hover colors

--- src/components/Homepage/BlockContainer.jsx
+++ src/components/Homepage/BlockContainer.jsx
@@ -29,7 +29,7 @@ export default function BlockContainer() {
         <div class="block-container">
             <Block title="Film & TV" rotate="none" bgcolor="#DCFF4E" link="/film-tv/" translate={isMobile ? '-3rem' : '0 -10rem'} blockSize={blockSize}  />
             <Block title="Music Videos" rotate="10deg" bgcolor="#6BFFF6" link="/music-videos/" translate={isMobile ? '-1rem' : '0 -2rem'} blockSize={blockSize} />
-            <Block title="Radio" rotate="20deg" bgcolor="#DCFF4E" link="/radio/" translate={isMobile ? 'none' : '0 8rem'} blockSize={blockSize} />
+            <Block title="Radio" rotate="20deg" bgcolor="#ffc61a" link="/radio/" translate={isMobile ? 'none' : '0 8rem'} blockSize={blockSize} />
             <Block title="Photos" rotate="-5deg" bgcolor="#B1FF4E" link="https://substack.com/@katetown" translate={isMobile ? '2rem' : '0 4rem'} blockSize={blockSize} />
         </div>
     )
