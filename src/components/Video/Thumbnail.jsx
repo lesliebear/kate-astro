2ad7c82940944a2cae95dbdce706f8bbf74b7d64
lesliebear <tianqi.xiong@gmail.com>
2025-02-20 14:21:47 -0500

    alt

--- src/components/Video/Thumbnail.jsx
+++ src/components/Video/Thumbnail.jsx
@@ -2,7 +2,7 @@ export default function Thumbnail({ imgSrc, title, showTitle }) {
     return (
         <div class="thumbnail">
             <a href={`/videos/${title}`}>
-                <img src={imgSrc} width={382} height={215} alt={title} />
+                <img src={imgSrc} width={382} height={215} alt="" />
                 {showTitle && (
                     <p class="caption">{title}</p>
                 )}
