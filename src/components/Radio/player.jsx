7b606a3fa44b9ab6689f75f82dcb87626948f0dd
lesliebear <tianqi.xiong@gmail.com>
2025-03-19 17:02:21 -0400

    created radio player component

---
+++ src/components/Radio/player.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Player({src, width="100%", height="166"}) {
+    return (
+        <iframe
+        width={width}
+        height={height}
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src={src}
+    ></iframe>
+    );
+}
