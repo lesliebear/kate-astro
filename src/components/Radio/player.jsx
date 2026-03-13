818ec50b779c75f49e0d3aca70e22aa51b15da03
lesliebear <tianqi.xiong@gmail.com>
2026-03-13 00:08:16 -0400

    added vertical margins to radio players

--- src/components/Radio/player.jsx
+++ src/components/Radio/player.jsx
@@ -1,12 +1,16 @@
-export default function Player({src, width="100%", height="166"}) {
-    return (
-        <iframe
+import "../../styles/player.css";
+
+export default function Player({ src, width = "100%", height = "166" }) {
+  return (
+    <div class="radio-player-wrapper">
+      <iframe
         width={width}
         height={height}
         scrolling="no"
         frameborder="no"
         allow="autoplay"
         src={src}
-    ></iframe>
-    );
+      ></iframe>
+    </div>
+  );
 }
